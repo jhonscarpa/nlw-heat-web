91f03da0f912730391e758799226239a72589d30
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-10-20 22:58:19 -0300

    integração com banco

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,7 +1,56 @@
+import { api } from '../../services/api'
+import styles from './styles.module.scss'
+import logoImg from '../../assets/logo.svg'
+import { useEffect } from 'react'
+
 export function MessageList() {
+  useEffect(() => {
+    api.get('messages/last3').then(response => {
+      console.log(response.data)
+    })
+  }, [])
+
   return (
-    <div>
-      <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImg} alt="DoWhile 2021" />
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jhonscarpa.png" alt="" />
+            </div>
+            <span>Jhonatan Scarpa</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jhonscarpa.png" alt="" />
+            </div>
+            <span>Jhonatan Scarpa</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>
+            Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+            de todos os tempos, vamooo pra cima! 🔥🔥
+          </p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/jhonscarpa.png" alt="" />
+            </div>
+            <span>Jhonatan Scarpa</span>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
