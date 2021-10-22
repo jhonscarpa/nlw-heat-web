f580b0f176b08971b5ce434594330c8b44daf2a8
jhonscarpa <scarpa.jhonatan@outlook.com>
2021-10-21 22:31:36 -0300

    finish

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -5,8 +5,7 @@ import { AuthContext } from '../../context/Auth'
 import styles from './styles.module.scss'
 
 export function LoginBox() {
-  const { signInUrl, user } = useContext(AuthContext)
-  console.log(user)
+  const { signInUrl } = useContext(AuthContext)
 
   return (
     <div className={styles.loginBoxWrapper}>
--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@ import { api } from '../../services/api'
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
 import { useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
 
 type Message = {
   id: string
@@ -12,11 +13,33 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = []
+
+const socket = io('http://localhost:4000')
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage)
+})
+
 export function MessageList() {
-  const [messages, setMessases] = useState<Message[]>([])
+  const [messages, setMessages] = useState<Message[]>([])
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState =>
+          [...messages, messagesQueue[0], prevState[0], prevState[1]].filter(
+            Boolean,
+          ),
+        )
+        messagesQueue.shift()
+      }
+    }, 3000)
+  }, [])
+
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
-      setMessases(response.data)
+      setMessages(response.data)
     })
   }, [])
 
@@ -24,9 +47,9 @@ export function MessageList() {
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
       <ul className={styles.messageList}>
-        {messages.map(data => {
+        {messages.map((data, index) => {
           return (
-            <li className={styles.message} key={data.id}>
+            <li className={styles.message} key={index}>
               <p className={styles.messageContent}>{data.text}</p>
               <div className={styles.messageUser}>
                 <div className={styles.userImage}>
